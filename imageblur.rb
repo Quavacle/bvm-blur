38737b5484ddc0888bce1f3f16a3bf1fc16e5a72
Bronson Valiquet Myers <owd.bob@gmail.com>
2019-08-03 15:37:17 -0700

    Amount of blur added

--- imageblur.rb
+++ imageblur.rb
@@ -1,17 +1,55 @@
 class Image
   def initialize(contents)
-    @mainimage = contents
+    @main = contents
   end
 
   def output_image
-    @mainimage.each { |together| puts together.join( '')}
+    @main.each {|together| puts together.join('')} 
+  end
+
+  def blur(distance = 1)
+    distance.times do
+    locate
+    @location_array.each do |y, x|
+      @main[y -1][x] = 1 unless y == 0
+      @main[y +1][x] = 1 unless y == @main.count - 1
+      @main[y][x +1] = 1 unless x == @main.count
+      @main[y][x -1] = 1 unless x == 0
+    end
+   end
+ end
+
+  def locate
+    @location_array = []
+    @main.each_with_index do |r, y|
+      r.each_with_index do |value, x|
+        if value == 1
+          @location_array << [y, x]
+        end
+      end
+    end
+   
   end
 end
-image = Image.new([
-  [0, 0, 0, 0],
-  [0 ,1, 0 ,0],
-  [0, 0, 0, 1],
-  [0, 0, 0, 0]
-  ])
 
+image = Image.new([
+  [0, 0, 0, 0, 0, 0, 0,0,0,0,0],
+  [0 ,0, 0 ,0, 0 ,0, 0,0,0,0,0],
+  [0, 0, 0, 0, 0, 0, 0,0,0,0,0],
+  [0, 0, 0, 0, 0 ,0, 0,0,0,0,0],
+  [0, 0, 0, 0, 0 ,0, 0,0,0,0,0],
+  [0, 0, 0, 0, 0 ,1, 0,0,0,0,0],
+  [0, 0, 0, 0, 0 ,0, 0,0,0,0,0],
+  [0, 0, 0, 0, 0 ,0, 0,0,0,0,0],
+  [0, 0, 0, 0, 0 ,0, 0,0,0,0,0],
+  [0, 0, 0, 0, 0 ,0, 0,0,0,0,0],
+
+])
+puts "-----------------"
+puts "--Initial Image--"
 image.output_image
+puts " "
+image.blur
+puts "--Blurred Image--"
+image.output_image
+puts "-----------------"
